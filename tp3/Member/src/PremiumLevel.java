2199aabb3ff6c92b0779967c2daa770bf4d9ae83
Sow Mamadou Radjaye <mamadou-radjaye.sow.etu@a15p1.fil.univ-lille.fr>
2026-09-22 11:03:14 +0200

    ajout de PremiumLevel

---
+++ tp3/Member/src/PremiumLevel.java
@@ -0,0 +1,20 @@
+public  enum PremiumLevel {argent,or,platine;
+    private static final int  seuileOr = 1000;
+    private  static final int seuilePlantine = 2000;
+
+    public static  PremiumLevel frompoints(int valeur){
+        if (valeur<seuileOr){
+            return argent;
+        } else if (valeur>seuileOr){
+            return platine;
+        } else
+        {
+            return or;
+        }
+
+    }
+
+
+}
+
+
